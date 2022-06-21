f3afc19133ab301eaeb9214f1fb23797542788a3
Juan López Rubio <j.lopez.r@gmail.com>
2022-06-21 13:31:01 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+import 'widget_view.dart';
+//import 'classic.dart';
+
+void main() {
+  runApp(const MyApp());
+}
---
+++ lib/widget_view.dart
@@ -0,0 +1,116 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:widget_view/widget_view.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'package:widget_view Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const MyHomePage(title: 'package:widget_view Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  MyHomePageController createState() => MyHomePageController();
+}
+
+class MyHomePageController extends State<MyHomePage> {
+  int counter = 0;
+
+  void incrementCounter() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (kIsWeb) {
+      return MyHomeExtraPageView(this);
+    } else {
+      return MyHomePageView(this);
+    }
+  }
+}
+
+class MyHomeExtraPageView
+    extends StatefulWidgetView<MyHomePage, MyHomePageController> {
+  const MyHomeExtraPageView(MyHomePageController controller, {Key? key})
+      : super(controller, key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '${controller.counter}',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: controller.incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class MyHomePageView
+    extends StatefulWidgetView<MyHomePage, MyHomePageController> {
+  const MyHomePageView(MyHomePageController controller, {Key? key})
+      : super(controller, key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Has pulsado el botoncico:',
+            ),
+            Text(
+              '${controller.counter}',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: controller.incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
